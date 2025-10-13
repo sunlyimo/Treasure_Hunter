ecaf4e75c840dca6e65d2269d9e5fe22246de388
sunlyimo <titouanmacre@gmail.com>
2025-10-13 16:31:18 +0200

    Ajout de l'image, de css, et la base de js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-const canvas = document.getElementById('canvas')
+const img = document.getElementById('carte')
+ 
+
+
+img.onload = function() {
+    img.addEventListener('click', onClickEvent);
+}
+
+function onClickEvent(evt) {
+    console.log(evt)
+}
 
