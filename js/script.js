8f837b048bef78bd8b57a5f389f7080a6a2d1f46
sunlyimo <titouanmacre@gmail.com>
2025-10-13 17:06:20 +0200

    Jeu fonctionnel dans la console

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-const img = document.getElementById('carte')
- 
+const mapImg = document.getElementById('map')
+const startButton = document.getElementById('start-button')
+
+let treasurePos = { x: 0, y: 0 }
+let nbClicksLeft = 0;
+
+mapImg.addEventListener('click', (evt) => {
+    nbClicksLeft--;
+    if (nbClicksLeft === 0) return loseGame();
+
+    const magnitude = Math.abs(treasurePos.x - evt.clientX) + Math.abs(treasurePos.y - evt.clientY);
+    if (magnitude < 30) {
+        console.log("gagné")
+    } else if (magnitude < 60) {
+        console.log("brûlant!")
+    } else if (magnitude < 120) {
+        console.log("chaud")
+    } else if (magnitude < 240) {
+        console.log("tiède")
+    } else {
+        console.log("glacé")
+    }
+});
 
 
-img.onload = function() {
-    img.addEventListener('click', onClickEvent);
+startButton.addEventListener('click', (evt) => {
+    treasurePos.x = mapImg.x + Math.floor(Math.random() * 131864861 % mapImg.clientWidth);
+    treasurePos.y = mapImg.y + Math.floor(Math.random() * 764653156 % mapImg.clientHeight);
+    nbClicksLeft = 15;
+});
+
+
+function loseGame() {
+
 }
-
-function onClickEvent(evt) {
-    console.log(evt)
-}
-
