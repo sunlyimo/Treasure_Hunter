7970cfe01ca9565317327dd11b9e3688023a1a2b
Titouan Macré <titouanmacre@gmail.com>
2025-12-04 18:47:41 +0100

    Ajout de la mise en page et de 'froid'

--- js/script.js
+++ js/script.js
@@ -11,25 +11,33 @@ let gamePlaying = false;
 
 mapImg.addEventListener('click', (evt) => {
     if (!gamePlaying) return;
+    const rect = mapImg.getBoundingClientRect();
 
-    const magnitude = Math.abs(treasurePos.x - evt.clientX) + Math.abs(treasurePos.y - evt.clientY);
+    // Calcule les coordonnées relatives à l'image
+    const x = evt.clientX - rect.left;
+    const y = evt.clientY - rect.top;
 
+    // Utilise ces coordonnées pour la logique du jeu
+    const magnitude = Math.abs(treasurePos.x - x) + Math.abs(treasurePos.y - y);
     if (magnitude < 30) { // Le click est assé proche du trésors, on gagne.
         addEmojiOnMap(treasurePos, '💰');
         giveHint("Gagné");
         winGame();
         return;
     } else if (magnitude < 60) {
-        addEmojiOnMap({ x: evt.clientX, y: evt.clientY }, '🔥');
+        addEmojiOnMap({ x: x, y: y }, '🔥');
         giveHint("Brûlant!")
     } else if (magnitude < 120) {
-        addEmojiOnMap({ x: evt.clientX, y: evt.clientY }, '🌡️');
+        addEmojiOnMap({ x: x, y: y }, '🌡️');
         giveHint("Chaud")
     } else if (magnitude < 240) {
-        addEmojiOnMap({ x: evt.clientX, y: evt.clientY }, '🗿');
+        addEmojiOnMap({ x: x, y: y }, '🗿');
         giveHint("Tiède")
+    } else if (magnitude < 300) {
+        addEmojiOnMap({ x: x, y: y }, '🍨');
+        giveHint("Froid")
     } else {
-        addEmojiOnMap({ x: evt.clientX, y: evt.clientY }, '🧊');
+        addEmojiOnMap({ x: x, y: y }, '🧊');
         giveHint("Glacé")
     }
 
@@ -42,15 +50,17 @@ mapImg.addEventListener('click', (evt) => {
     }
 });
 
-
-startButton.addEventListener('click', () => {
-    treasurePos.x = mapImg.x + Math.floor(Math.random() * 131864861 % mapImg.clientWidth);
-    treasurePos.y = mapImg.y + Math.floor(Math.random() * 764653156 % mapImg.clientHeight);
+function startGame() {
+    treasurePos.x = Math.floor(Math.random() * 131864861 % mapImg.clientWidth);
+    treasurePos.y = Math.floor(Math.random() * 764653156 % mapImg.clientHeight);
     nbClicksLeft = MAX_NB_CLICS;
     gamePlaying = true;
     removeEmojisOnMap();
-    giveHint('');
-});
+    giveHint('Trouvez le trésors...');
+}
+
+
+startButton.addEventListener('click', startGame);
 
 
 function giveHint(hint) {
@@ -71,10 +81,8 @@ function addEmojiOnMap(pos, emoji) {
     newLabel = document.createElement("label");
     newLabel.textContent = emoji;
     mapImg.style.position = 'relative';
-    newLabel.style.pointerEvents = 'none';
-    newLabel.style.userSelect = 'none';
+    newLabel.className = "treasure";
     mapDiv.appendChild(newLabel)
-    newLabel.style.position = 'absolute';
     newLabel.style.left = `${pos.x - newLabel.offsetWidth / 2}px`;
     newLabel.style.top = `${pos.y - newLabel.offsetHeight / 2}px`;
 }
@@ -87,3 +95,7 @@ function removeEmojisOnMap() {
         mapDiv.removeChild(children[i]);
     }
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    startGame();
+});
